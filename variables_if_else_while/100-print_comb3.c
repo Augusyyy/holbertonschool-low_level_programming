df5d9abf017942146d7ccc3e19456470f4971d07
Augusyyy <8851@holbertonstudents.com>
2024-02-18 06:25:37 -0800

    100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - 10-print_comb2.c
+ *
+  *
+ * Return: Always 0.
+ */
+int main(void) {
+
+    for (int a= '0'; a < '9'; a++) {
+        for (int b= '0'; b < '9'; b++){
+            putchar(a);
+            putchar(b);
+
+        if (a=='9' && b=='9'){
+                putchar(',');
+                putchar(' ');
+            }
+
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
+}
