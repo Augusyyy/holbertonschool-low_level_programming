076a5e00a5868990a320aa082f204bf4cc5ed505
Augusyyy <8851@holbertonstudents.com>
2024-02-18 06:29:12 -0800

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,10 @@ int main(void) {
             putchar(a);
             putchar(b);
 
-        if (a=='9' && b=='9'){
-                putchar(',');
-                putchar(' ');
-            }
-
+        if (a!='9' && b!='9') {
+            putchar(',');
+            putchar(' ');
+        }
         }
     }
 
