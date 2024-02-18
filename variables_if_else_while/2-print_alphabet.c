4be977844963be97515898be947c35095e81da3f
Augusyyy <8851@holbertonstudents.com>
2024-02-17 19:31:08 -0800

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-    putchar('\n');
-
-    return (0);
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	
+	return (0);
 }
