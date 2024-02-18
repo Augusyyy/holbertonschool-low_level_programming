ec5c54ca1d3ce30499ecd18e10a8d1a329636438
Augusyyy <8851@holbertonstudents.com>
2024-02-18 06:52:15 -0800

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@
 int main(void) {
 	int a,b;
 
-    for (a= '0'; a <= '9'; a++) {
+    for (a= '0'; a < '9'; a++) {
         for (b=a+1; b <= '9'; b++){
             putchar(a);
             putchar(b);
