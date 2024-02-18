57e7686f5a0fef8eb636077714a02f9ac48ca2ed
Augusyyy <8851@holbertonstudents.com>
2024-02-17 20:05:08 -0800

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - 4. alphABET
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter !='q' && letter !='e')
+	{
+	putchar(letter);
+	}
+    }
+    putchar('\n');
+
+    return (0);
+}
