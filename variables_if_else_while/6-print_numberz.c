f5d3804b39c5f283460851fb5f19caba72d4ee4d
Augusyyy <8851@holbertonstudents.com>
2024-02-17 23:02:45 -0800

    5-print_numbers.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,13 +6,14 @@
  *
  * Return: Always 0.
  */
-int main(void) {
-    signed int a;
+int main(void)
+{
+	signed int a;
 
-    for (a = '0'; a <= '9'; a++)
-            putchar(a);
+	for (a = '0'; a <= '9'; a++);
+	putchar(a);
 
     putchar('\n');
-
+    
     return (0);
 }
