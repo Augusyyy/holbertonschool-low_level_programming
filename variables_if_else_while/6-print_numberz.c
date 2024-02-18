a7840f2269befa0bc8f492178327f947a68e8e66
Augusyyy <8851@holbertonstudents.com>
2024-02-17 20:16:45 -0800

    4-print_alphabt.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - 5. Numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for(int a = 0;a <= 9 ; a++)
+    {
+	printf("%d", a);
+    }
+
+    return (0);
+}
