f5ea047b1ad314040fcebe5726a223ade2e9eb6d
Augusyyy <8851@holbertonstudents.com>
2024-02-19 15:53:28 -0800

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -3,20 +3,19 @@
 /**
  * main - 8-Hexadecimal
  *
-  *
+ *
  * Return: Always 0.
  */
-int main(void) {
-    signed int a;
-    signed int c;
+int main(void)
+{
+       	signed int a;
+	signed int c;
 
-    for (a = '0'; a <= '9'; a++)
-            putchar(a);
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
 
-    for (c = 'a'; c <= 'f'; c++)
-        putchar(c);
-
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
