2473877c56d40bd0b10acf3f54d9a6b4193b3c60
Augusyyy <8851@holbertonstudents.com>
2024-02-19 15:47:11 -0800

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -3,16 +3,15 @@
 /**
  * main - 6. Numberz
  *
-  *
+ *
  * Return: Always 0.
  */
-int main(void) {
-    signed int a;
-
-    for (a = '0'; a <= '9'; a++)
-            putchar(a);
-
-    putchar('\n');
-
-    return (0);
+int main(void)
+{
+	signed int a;
+	
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
 }
