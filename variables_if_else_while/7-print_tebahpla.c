7e248fd831f9f76184c2b3af40bd01ea3bb12c87
Augusyyy <8851@holbertonstudents.com>
2024-02-19 15:50:51 -0800

    7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -3,16 +3,15 @@
 /**
  * main - 7-print_tebahpla
  *
-  *
+ *
  * Return: Always 0.
  */
-int main(void) {
-    signed int letter;
+int main(void)
+{
+	signed int letter;
 
-    for (letter = 'z'; letter >= 'a'; letter--)
-        putchar(letter);
-
-    putchar('\n');
-
-    return (0);
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
 }
