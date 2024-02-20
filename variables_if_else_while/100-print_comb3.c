7f61272391e100872cefeb693f7a20c2ef532e34
Augusyyy <8851@holbertonstudents.com>
2024-02-19 16:38:16 -0800

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,20 @@ int main(void)
 {
 	int i;
 	int j;
-
+	
 	for (i = 0; i < 9; i++)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
-			putchar('0' + i); // 打印第一个数字
-			putchar('0' + j); // 打印第二个数字
+			putchar('0' + i);
+			putchar('0' + j);
 			if (i != 8 || j != 9)
 			{
-				putchar(','); // 打印逗号
-				putchar(' '); // 打印空格
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar('\n'); // 换行
+	putchar('\n');
 	return (0);
 }
