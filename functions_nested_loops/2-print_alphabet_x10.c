24e20f1ea23e59608c256ede158386b98cbcc1e4
Augusyyy <8851@holbertonstudents.com>
2024-02-20 07:36:47 -0800

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,12 @@
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
-void print_alphabet_x10(void){
+void print_alphabet_x10(void)
+{
 	char letter;
 	int i;
-	
-	for(i = 0; i < 10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
