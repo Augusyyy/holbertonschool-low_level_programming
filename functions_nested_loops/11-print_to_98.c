c819af13db42e039766c37ed3fa4e57d3e2f499d
Augusyyy <8851@holbertonstudents.com>
2024-02-23 01:26:45 +1100

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,11 +13,11 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d",n++);
+				printf("%d", n++);
 			}
 			else
 			{
-				printf ("%d, ",n++);
+				printf ("%d, ", n++);
 			}
 		}
 		printf ("\n");
@@ -28,11 +28,11 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d",n--);
+				printf("%d", n--);
 			}
 			else
 			{
-				printf ("%d, ",n--);
+				printf ("%d, ", n--);
 			}
 		}
 		printf ("\n");
