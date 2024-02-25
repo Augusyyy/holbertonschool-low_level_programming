269967f10ee36b3f6bfbf5350c8e4a78a6c12d81
Augusyyy <8851@holbertonstudents.com>
2024-02-25 02:30:48 +0000

    1-isdigit.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,25 +1,19 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 /*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
+ * File: main.h
+ * Auth: Augusyyyn
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
  */
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
