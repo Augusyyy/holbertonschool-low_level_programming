6b58acc7d725c21fa25250cc645fe67d13ec33ca
Augusyyy <8851@holbertonstudents.com>
2024-02-25 13:31:02 +0000

    100-prime_factor.c

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the largest prime
+ *        factor of the number 612852475143.
+ *
+ * Return: Always 0.
+ */
+void main(void)
+{
+	long liugod = 612852475143;
+	long maxPrimeResult = 0;
+	
+	while (liugod > 1)
+	{
+		for (long a = 2;a <= liugod; a++)
+		{
+			if (liugod % a == 0)
+			{
+				liugod = liugod/a;
+				if (a > maxPrimeResult)
+				{
+					maxPrimeResult = a;
+				}
+				break;
+			}
+		}
+	}
+	printf("%ld\n", maxPrimeResult);
+	return (0);
+}
