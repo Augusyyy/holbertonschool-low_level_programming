6ead363501b79fc32e6f4691fb0e98cc1a6e52d9
Augusyyy <8851@holbertonstudents.com>
2024-02-28 09:18:05 +0000

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", *a);
 		if (i != n - 1)
 		{
-			printf(", ", *a);
+			printf(", ");
 			a++;
 		}
 	}
