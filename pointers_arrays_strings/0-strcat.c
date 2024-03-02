f7bb59665d15f1b4bab061fbc4b625f49d29ee8d
Augusyyy <8851@holbertonstudents.com>
2024-03-02 14:57:23 +0000

    0n'

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminating
@@ -26,4 +25,3 @@ char *_strcat(char *dest, char *src)
 	*p = '\0';
 	return  dest;
 }
-
