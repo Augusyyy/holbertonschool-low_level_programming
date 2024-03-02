20c9eccc4bfebe2e2c6f60360ec68a9678fa6447
Augusyyy <8851@holbertonstudents.com>
2024-03-02 14:49:48 +0000

    nmain

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,32 +1,20 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/**
- * File: main.h
- * Auth: Augusyyy
- * Desc: Header file contain  all functions identify
- *       used in the pointers_arrays_strings directory.
- */
-
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
@@ -36,4 +24,3 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 #endif
-
