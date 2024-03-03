f31dc921df7235c9f762482b9fbfa775397cdead
Augusyyy <8851@holbertonstudents.com>
2024-03-03 07:51:31 +0000

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reverse_array - function for reversing
+* @a: array
+* @n: value
+*/
+void reverse_array(int *a, int n)
+{
+	int len = n;
+	int *p = a;
+	int *q = a;
+	int temp;
+
+	for (int i = 0; i < n; i++)
+	{
+		q++;
+	}
+	q--;
+	while (len > 1)
+	{
+		temp = *p;
+		*p = *q;
+		*q = temp;
+		p++;
+		q--;
+		len = len - 2;
+	}
+}
