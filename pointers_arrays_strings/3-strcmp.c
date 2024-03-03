9af3d8b53150d18e6c755cf7fe7b42b2420956be
Augusyyy <8851@holbertonstudents.com>
2024-03-03 07:38:57 +0000

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,10 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	char p1,p2;
+	char p1, p2;
 
 	do
-	{
-		p1 = *s1;
+	{p1 = *s1;
 		p2 = *s2;
 		s1++;
 		s2++;
@@ -20,7 +19,6 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (p1 - p2);
 		}
-	}
-	while (p1 == p2);
+	}while (p1 == p2);
 	return (p1 - p2);
 }
