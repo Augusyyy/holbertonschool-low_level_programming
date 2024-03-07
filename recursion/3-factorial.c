e9e5193b06c5453f20b113b76c6b0486d7246064
Augusyyy <8851@holbertonstudents.com>
2024-03-07 15:12:17 +0000

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - Calculate the factorial of a number
+ * @n: The number to find the factorial of.
+ * Return: If n > 0 - the factorial of n.
+ *         If n < 0 - 1 to indicate an error.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
