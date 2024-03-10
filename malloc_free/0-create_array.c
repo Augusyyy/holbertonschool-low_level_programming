107c8e1c497c48969e960e4d947d17645ad970a6
Augusyyy <8851@holbertonstudents.com>
2024-03-10 14:57:05 +0000

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -20,7 +20,6 @@ char *create_array(unsigned int size, char c)
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
-	memset(array, 0, sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
