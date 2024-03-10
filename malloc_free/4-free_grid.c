891ef53ad3f1f02dc7dad3c9b8e4904a42adbee1
Augusyyy <8851@holbertonstudents.com>
2024-03-10 15:29:40 +0000

    alloc_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -8,11 +8,11 @@
  */
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
     for (i = 0; i < height; i++)
     {
-        free(grid[i]);
+	    free(grid[i]);
     }
 
     free(grid);
