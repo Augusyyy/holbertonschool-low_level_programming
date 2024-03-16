400e393aed4fdfad2adb2b6a68476cbeae7bfea4
Augusyyy <8851@holbertonstudents.com>
2024-03-16 14:50:40 +0000

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,14 +1,20 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name.
- * @name: The name to be printed.
- * @f: A pointer to a function that prints a name.
+ * array_iterator - Executes a function given as a
+ *                  parameter on each element of an array.
+ * @array: The array.
+ * @size: The size of array.
+ * @action: A pointer to the function to be executed.
  */
-void print_name(char *name, void (*f)(char *))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (name == NULL || f == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
-	f(name);
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 }
