aead7949724cab4d571d4489acedba83347fc9be
Augusyyy <8851@holbertonstudents.com>
2024-04-07 15:55:06 +0000

    gosomething

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,12 +13,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
-	while (*p)
+	if (text_content != NULL)
 	{
-		len++;
-		p++;
+		while (*p)
+		{
+			len++;
+			p++;
+		}
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,12 +15,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
-	while (*p)
+	if (text_content != NULL)
 	{
-		len++;
-		p++;
+		while (*p)
+		{
+			len++;
+			p++;
+		}
 	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
